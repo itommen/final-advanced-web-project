f0dcb190ad77df6c039c6f62edc5aea6a40ce2bf
stavl <stavlev95@gmail.com>
2018-03-06 22:09:16 +0200

    Implemented another bar chart to show the results of the GROUP BY query

--- client/app/statistics/statistics.controller.js
+++ client/app/statistics/statistics.controller.js
@@ -4,7 +4,7 @@ const CONTROLLER = 'statisticsController';
 
 angular.module('advanced.controllers')
   .controller(CONTROLLER, ($scope, $state, Post, LoggedUser) => {
-    //LoggedUser.ensureLogged();
+    LoggedUser.ensureLogged();
 
     $scope.createDetailedRecipesGraph = () => {
       const margin = { top: 20, right: 30, bottom: 150, left: 40 };
@@ -76,49 +76,71 @@ angular.module('advanced.controllers')
     };
 
     $scope.createContributingUsersGraph = () => {
-      var width = 400,
-        height = 400,
-        radius = Math.min(width, height) / 2;
+      const margin = { top: 20, right: 30, bottom: 150, left: 40 };
+      const width = 800 - margin.left - margin.right;
+      const height = 400 - margin.top - margin.bottom;
 
-      var color = d3.scale.ordinal()
-        .range(["#a05d56", "#6b486b", "#ff8c00", "#98abc5", "#8a89a6", "#d0743c", "#7b6888"]);
-      debugger;
+      // set the ranges
+      const x = d3.scale.ordinal().rangeRoundBands([0, width], 0.4);
+      const y = d3.scale.linear().range([height, 0]);
 
-      var arc = d3.svg.arc()
-        .outerRadius(radius - 10)
-        .innerRadius(0);
+      var xAxis = d3.svg.axis().scale(x).orient("bottom");
+      var yAxis = d3.svg.axis().scale(y).orient("left").ticks(10);
 
+      // add the SVG element
       var svg = d3.select("#contributing-users-graph").append("svg")
-        .attr("width", width)
-        .attr("height", height)
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
         .append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+      // get the recipes' data
       Post.byUsername().$promise
-        .then((data) => {
-          debugger;
-          var pie = d3.layout.pie()
-            .sort(null)
-            .value(({ count }) => count);
-
+        .then(result => {
+          let data = result;
           data.forEach(function (d) {
-            d.count = +d.count;
+            d.Username = d._id;
+            d.NumberOfPosts = + d.count;
           });
 
-          var g = svg.selectAll(".arc")
-            .data(pie(data))
-            .enter().append("g")
-            .attr("class", "arc");
+          Promise.resolve()
+            .then(() => {
+              // scale the range of the data
+              x.domain(data.map(d => { return d.Username; }));
+              y.domain([0, d3.max(data, d => { return d.NumberOfPosts; })]);
 
-          g.append("path")
-            .attr("d", arc)
-            .style("fill", ({ _id }) => color(_id));
+              // add axis
+              svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis)
+                .selectAll("text")
+                .style("text-anchor", "end")
+                .attr("dx", "-.8em")
+                .attr("dy", "-.55em")
+                .attr("transform", "rotate(-90)");
 
-          g.append("text")
-            .attr("transform", function (d) { return "translate(" + arc.centroid(d) + ")"; })
-            .attr("dy", ".35em")
-            .style("text-anchor", "middle")
-            .text(({ _id }) => _id);
+              svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 5)
+                .attr("dy", ".71em")
+                .style("text-anchor", "end")
+                .text("NumberOfPosts");
+
+              // Add bar chart
+              svg.selectAll("bar")
+                .data(data)
+                .enter()
+                .append("rect")
+                .attr("class", "bar")
+                .attr("x", function (d) { return x(d.Username); })
+                .attr("width", x.rangeBand())
+                .attr("y", function (d) { return y(d.NumberOfPosts); })
+                .attr("height", function (d) { return height - y(d.NumberOfPosts); });
+            });
         });
     };
   });
--- server/api/post/post.seed.js
+++ server/api/post/post.seed.js
@@ -6,7 +6,13 @@ export default {
       In a large bowl, cream together the butter, peanut butter, brown sugar, and white sugar until smooth. Beat in the eggs one at a time, then stir in the corn syrup, water, and vanilla. Combine the flour, baking soda, and salt; stir into the peanut butter mixture. Fold in chocolate chunks. Drop by 1/4 cupfuls 3 inches apart onto ungreased baking sheets.
       Bake for 12 to 14 minutes in the preheated oven, or until edges are golden. Allow cookies to cool for 1 minute on the cookie sheet before removing to wire racks to cool completely.`,
       date: new Date('December 17, 2017 17:24:00'),
-      author: 'Dana Eliyahu'
+      author: 'dana'
+    }, {
+      title: 'Labneh (Lebanese Yogurt)',
+      content: `Mix Greek yogurt, olive oil, mint, dill, and kosher salt together in a bowl.
+      Cover and refrigerate up to 12 hours.`,
+      date: new Date('January 22, 2018 08:45:42'),
+      author: 'dana'
     }, {
       title: 'Macaroni and Cheese',
       content: `Preheat an oven to 350 degrees F (175 degrees C). Grease a 9x13 inch glass baking dish.
@@ -14,7 +20,20 @@ export default {
       While the macaroni is boiling, melt the remaining butter in a saucepan over medium heat. Stir in the onions, and cook until the onions begin to soften, about 3 minutes. Stir in the flour, and cook 5 minutes longer. Add the milk and bring to a simmer, stirring frequently. Cook and stir until the milk has thickened, about 10 minutes. Once thick and smooth, stir in the salt, white pepper, Worcestershire sauce, hot pepper sauce, mustard, 2 cups of Cheddar cheese, and 1/4 cup of Parmesan cheese. Stir until the cheeses have melted, then stir in the macaroni until evenly coated. Scrape into the prepared baking dish, and smooth the top. Toss the remaining 1 cup Cheddar cheese and 1/4 cup Parmesan cheese with the dry bread crumbs in a mixing bowl. Sprinkle evenly over the top of the macaroni.
       Bake in the preheated oven until the macaroni is hot and the crust is golden brown, about 30 minutes.`,
       date: new Date('January 5, 2018 18:00:03'),
-      author: 'Shenhav Meshulam'
+      author: 'shenhav'
+    }, {
+      title: 'Meatballs',
+      content: `Combine ground pork, Italian sausage, ground sirloin, and ground chuck in a large bowl. Mix them with your hands until thoroughly combined.
+      Soak bread chunks in chicken broth in a bowl until completely soft. Combine garlic and parsley in a mini chopper or food processor and grind to a fine paste. Squeeze the excess broth out of the bread, reserving the broth in a bowl, and add the bread to the meats along with the garlic and parsley paste. Gently mix in beaten eggs and grated Pecorino Romano cheese with your hands until fully incorporated into the meat mixture. If the mixture seems dry, pour in some of the reserved chicken broth.
+      Pinch off golf ball-sized pieces of the meat mixture and roll them into meatballs, taking care not to pack them too tightly, and set them on a baking tray. Depending on how big you make them, you should end up with about 30 meatballs.
+      Heat the olive oil and butter in a large skillet over medium-high heat. Working in two or three batches, brown the meatballs on all sides and drain on paper towels. If you're adding them to a pot of sauce, you may undercook them slightly, as they will finish cooking in the sauce.`,
+      date: new Date('January 5, 2018 18:05:00'),
+      author: 'shenhav'
+    }, {
+      title: 'Israeli Salad',
+      content: `Toss cucumbers, tomatoes, onion, bell pepper, garlic, parsley, and mint together in a bowl. Drizzle olive oil and lemon juice over the salad and toss to coat; season with salt and pepper.`,
+      date: new Date('January 5, 2018 18:07:00'),
+      author: 'shenhav'
     }, {
       title: 'Roasted Cauliflower Soup',
       content: `Place the cauliflower florets into a large bowl of lightly salted water; allow to stand for 20 minutes. Drain well, and arrange on a sheet of heavy aluminum foil on a baking sheet. Spray the olive oil cooking spray evenly on the cauliflower.
@@ -22,7 +41,7 @@ export default {
       Broil the cauliflower until browned, 20 to 30 minutes.
       Meanwhile, heat olive oil in a large soup pot, and cook the onion until translucent, about 5 minutes; stir in the garlic and roasted cauliflower. Pour in the water, season with salt and black pepper, and simmer until all the vegetables are tender, about 30 minutes. Blend the soup in the pot with an immersion hand blender until creamy and smooth.`,
       date: new Date('February 14, 2018 12:03:00'),
-      author: 'Tom Mendelson'
+      author: 'tom'
     }, {
       title: 'Chocolate Sheet Cake',
       content: `Preheat an oven to 350 degrees F (175 degrees C). Grease an 11x17 jelly roll pan.
@@ -30,8 +49,7 @@ export default {
       Bake in preheated oven until a toothpick inserted into the center of cake comes out clean, 20 to 25 minutes. Remove from oven and allow to cool on a wire rack.
       Combine remaining 1/2 cup butter, 3 tablespoons cocoa powder, and milk in a small saucepan; stir over medium heat until butter is melted and mixture is well combined. Beat powdered sugar into the butter mixture, mixing until frosting is smooth. Stir pecans into frosting and pour over cooled cake.`,
       date: new Date('March 1, 2018 07:38:16'),
-      author: 'Stav Levinger'
-    }, {
+      author: 'stav'
     }];
   }
 };

1bdafd0fa6c19b4927c777f244d66baf67920422
stavl <stavlev95@gmail.com>
2018-03-06 17:36:48 +0200

    Tries to start implementing pie chart with contributing users stats, still need to change the use of d.Count + add a group-by query using Mongoose

--- client/app/statistics/statistics.controller.js
+++ client/app/statistics/statistics.controller.js
@@ -74,6 +74,53 @@ angular.module('advanced.controllers')
             });
         });
     };
+
+    $scope.createContributingUsersGraph = () => {
+      var width = 400,
+        height = 400,
+        radius = Math.min(width, height) / 2;
+
+      var color = d3.scale.ordinal()
+        .range(["#a05d56", "#6b486b", "#ff8c00", "#98abc5", "#8a89a6", "#d0743c", "#7b6888"]);
+
+      var arc = d3.svg.arc()
+        .outerRadius(radius - 10)
+        .innerRadius(0);
+
+      var svg = d3.select("#contributing-users-graph").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+      var data = Post.aggregate([
+        { $group: { _id: "$author", posts: { $push: "$title" } } }
+      ]);
+      debugger
+
+      var pie = d3.layout.pie()
+        .sort(null)
+        .value(function (d) { return d.Count; });
+
+      data.forEach(function (d) {
+        d.Count = + d.Count;
+      });
+
+      var g = svg.selectAll(".arc")
+        .data(pie(data))
+        .enter().append("g")
+        .attr("class", "arc");
+
+      g.append("path")
+        .attr("d", arc)
+        .style("fill", function (d) { return color(d.data.userName); });
+
+      g.append("text")
+        .attr("transform", function (d) { return "translate(" + arc.centroid(d) + ")"; })
+        .attr("dy", ".35em")
+        .style("text-anchor", "middle")
+        .text(function (d) { return d.data.userName; });
+    };
   });
 
 export default CONTROLLER;

543fe47edce493c9fc65888d45e4d744a8bb71db
shenhav <snhvm0@gmail.com>
2018-03-07 13:53:13 +0200

    added ajax

--- client/app/statistics/statistics.controller.js
+++ client/app/statistics/statistics.controller.js
@@ -4,9 +4,14 @@ const CONTROLLER = 'statisticsController';
 
 angular.module('advanced.controllers')
   .controller(CONTROLLER, ($scope, $state, Post, LoggedUser) => {
-    LoggedUser.ensureLogged();
+    //LoggedUser.ensureLogged();
 
     $scope.createDetailedRecipesGraph = () => {
+      // get the recipes' data
+      $.get('api/posts', onPostsLoaded);
+    };
+
+    function onPostsLoaded(data) {
       const margin = { top: 20, right: 30, bottom: 150, left: 40 };
       const width = 800 - margin.left - margin.right;
       const height = 400 - margin.top - margin.bottom;
@@ -25,57 +30,53 @@ angular.module('advanced.controllers')
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-      // get the recipes' data
-      Post.query().$promise
-        .then(result => {
-          let data = result;
-          data.forEach(function (d) {
-            d.Title = d.title;
-            d.Length = + d.content.length;
-          });
+      data.forEach(function (d) {
+        d.Title = d.title;
+        d.Length = + d.content.length;
+      });
 
-          Promise.resolve()
-            .then(() => {
-              // scale the range of the data
-              x.domain(data.map(d => { return d.Title; }));
-              y.domain([0, d3.max(data, d => { return d.Length; })]);
+      // scale the range of the data
+      x.domain(data.map(d => { return d.Title; }));
+      y.domain([0, d3.max(data, d => { return d.Length; })]);
 
-              // add axis
-              svg.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + height + ")")
-                .call(xAxis)
-                .selectAll("text")
-                .style("text-anchor", "end")
-                .attr("dx", "-.8em")
-                .attr("dy", "-.55em")
-                .attr("transform", "rotate(-90)");
+      // add axis
+      svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", "-.55em")
+        .attr("transform", "rotate(-90)");
 
-              svg.append("g")
-                .attr("class", "y axis")
-                .call(yAxis)
-                .append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 5)
-                .attr("dy", ".71em")
-                .style("text-anchor", "end")
-                .text("Length");
+      svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 5)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Length");
 
-              // Add bar chart
-              svg.selectAll("bar")
-                .data(data)
-                .enter()
-                .append("rect")
-                .attr("class", "bar")
-                .attr("x", function (d) { return x(d.Title); })
-                .attr("width", x.rangeBand())
-                .attr("y", function (d) { return y(d.Length); })
-                .attr("height", function (d) { return height - y(d.Length); });
-            });
-        });
-    };
+      // Add bar chart
+      svg.selectAll("bar")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", function (d) { return x(d.Title); })
+        .attr("width", x.rangeBand())
+        .attr("y", function (d) { return y(d.Length); })
+        .attr("height", function (d) { return height - y(d.Length); });
+    }
 
     $scope.createContributingUsersGraph = () => {
+      $.get('api/posts/byUsername', onPostsByUsernameLoaded);
+    }
+
+    function onPostsByUsernameLoaded(data) {
       const margin = { top: 20, right: 30, bottom: 150, left: 40 };
       const width = 800 - margin.left - margin.right;
       const height = 400 - margin.top - margin.bottom;
@@ -103,46 +104,43 @@ angular.module('advanced.controllers')
             d.NumberOfPosts = + d.count;
           });
 
-          Promise.resolve()
-            .then(() => {
-              // scale the range of the data
-              x.domain(data.map(d => { return d.Username; }));
-              y.domain([0, d3.max(data, d => { return d.NumberOfPosts; })]);
+          // scale the range of the data
+          x.domain(data.map(d => { return d.Username; }));
+          y.domain([0, d3.max(data, d => { return d.NumberOfPosts; })]);
 
-              // add axis
-              svg.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + height + ")")
-                .call(xAxis)
-                .selectAll("text")
-                .style("text-anchor", "end")
-                .attr("dx", "-.8em")
-                .attr("dy", "-.55em")
-                .attr("transform", "rotate(-90)");
+          // add axis
+          svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .selectAll("text")
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", "-.55em")
+            .attr("transform", "rotate(-90)");
 
-              svg.append("g")
-                .attr("class", "y axis")
-                .call(yAxis)
-                .append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 5)
-                .attr("dy", ".71em")
-                .style("text-anchor", "end")
-                .text("NumberOfPosts");
+          svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 5)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("NumberOfPosts");
 
-              // Add bar chart
-              svg.selectAll("bar")
-                .data(data)
-                .enter()
-                .append("rect")
-                .attr("class", "bar")
-                .attr("x", function (d) { return x(d.Username); })
-                .attr("width", x.rangeBand())
-                .attr("y", function (d) { return y(d.NumberOfPosts); })
-                .attr("height", function (d) { return height - y(d.NumberOfPosts); });
-            });
+          // Add bar chart
+          svg.selectAll("bar")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("class", "bar")
+            .attr("x", function (d) { return x(d.Username); })
+            .attr("width", x.rangeBand())
+            .attr("y", function (d) { return y(d.NumberOfPosts); })
+            .attr("height", function (d) { return height - y(d.NumberOfPosts); });
         });
-    };
+    }
   });
 
 export default CONTROLLER;
